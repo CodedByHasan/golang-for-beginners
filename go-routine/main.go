1fca302d0613a3ad14f86e3a3abd3b1abd2f9be1
Hasan <alkhashaab.h@gmail.com>
2024-10-02 23:22:56 +0930

    feat: added example of concurrent program

---
+++ go-routine/main.go
@@ -0,0 +1,30 @@
+// Concurrent program that calculates the square
+// of a number (from 1 to 10 000) and displays it
+// to the screen. This program takes less than 0.5
+// seconds to execute!
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func calculateSquare(i int) {
+    time.Sleep(1 * time.Second)
+    fmt.Println(i * i)
+}
+
+func main() {
+    start := time.Now()
+
+    for i := 1; i <= 10000; i++ {
+        // go routine
+        go calculateSquare(i)
+    }
+
+    elapsed := time.Since(start)
+    // need to Sleep for ~1 second to see output
+    // of calculateSquare
+    time.Sleep(1 * time.Second)
+    fmt.Println("Function took: ", elapsed)
+}
