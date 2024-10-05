d75462e2e2815bb1fca954f92ad7751e2c532cf7
Hasan <alkhashaab.h@gmail.com>
2024-10-05 11:25:44 +0930

    feat: added example of channel read and write

---
+++ channels/read-and-write/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "fmt"
+    "sync"
+)
+
+func main() {
+    var wg sync.WaitGroup
+    wg.Add(2)
+
+    channel := make(chan string)
+
+    go sell(channel, &wg)
+    go buy(channel, &wg)
+
+    wg.Wait()
+}
+
+// sends data to the channel
+func sell(channel chan string, wg *sync.WaitGroup) {
+    // sell() will BLOCK go routine until another channel receives data
+    channel <- "SOME DATA"
+    fmt.Println("Sent data to the channel")
+
+    wg.Done()
+}
+
+// receive data from the channel
+func buy(channel chan string, wg *sync.WaitGroup) {
+    fmt.Println("Waiting for data...")
+
+    // buy() will UNBLOCK sell() go routine
+    val := <-channel
+    fmt.Printf("Received data - %s\n", val)
+
+    wg.Done()
+}
