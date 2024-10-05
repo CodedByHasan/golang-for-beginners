2b0aa1538f444c3df45ab0882e6b2f67478687af
Hasan <alkhashaab.h@gmail.com>
2024-10-05 19:31:13 +0930

    feat: added example of spawing a goroutine closure in a loop

---
+++ channels/closure-in-a-loop/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    "fmt"
+    "sync"
+)
+
+func main() {
+    var wg sync.WaitGroup
+    wg.Add(10)
+
+    for i := 1; i <= 10; i++ {
+        // This is a closure (function within a function)
+        // pass in "i" as an argument so that it is well
+        // defined at the point of calling the goroutine
+        go func(i int) {
+            fmt.Println(i)
+            wg.Done()
+        }(i)
+    }
+
+    wg.Wait()
+    fmt.Println("Done")
+}
