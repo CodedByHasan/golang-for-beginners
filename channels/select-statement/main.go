c8b18ba3053cc439bb7bdc8e66e7b801484d9d46
Hasan <alkhashaab.h@gmail.com>
2024-10-05 17:23:03 +0930

    feat: updated select statement example to include default block and break statement

--- channels/select-statement/main.go
+++ channels/select-statement/main.go
@@ -18,8 +18,13 @@ func main() {
     select {
     case val1 := <-channel1:
         fmt.Println(val1)
+        break
     case val2 := <-channel2:
         fmt.Println(val2)
+        break
+    // Makes the select statement NON BLOCKING
+    default:
+        fmt.Println("Executed default block")
     }
     time.Sleep(1 * time.Second)
 }
