e87854d1b2f6ea27aedddc1176860ea1e7bf2422
Hasan <alkhashaab.h@gmail.com>
2024-10-05 17:15:51 +0930

    feat: add example of select statement used to select channel

---
+++ channels/select-statement/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func main() {
+    channel1 := make(chan string)
+    channel2 := make(chan string)
+
+    go goChannelOne(channel1)
+    go goChannelTwo(channel2)
+
+    // output is non-deterministic because
+    // select statement will select a
+    // channel at random
+    select {
+    case val1 := <-channel1:
+        fmt.Println(val1)
+    case val2 := <-channel2:
+        fmt.Println(val2)
+    }
+    time.Sleep(1 * time.Second)
+}
+
+func goChannelOne(channel1 chan string) {
+    channel1 <- "Channel 1"
+}
+
+func goChannelTwo(channel2 chan string) {
+    channel2 <- "Channel 2"
+}
