6dfdbd981d9a316baa999143273e0aadf4ba3984
Hasan <alkhashaab.h@gmail.com>
2024-10-07 00:05:19 +1030

    chore: updated comment inside closure-in-a-loop package.

--- channels/closure-in-a-loop/main.go
+++ channels/closure-in-a-loop/main.go
@@ -11,9 +11,11 @@ func main() {
     wg.Add(10)
 
     for i := 1; i <= 10; i++ {
-        // This is a closure (function within a function)
-        // pass in "i" as an argument so that it is well
-        // defined at the point of calling the goroutine
+
+        // This is a closure (function value that references
+        // variables from outside its body).Pass in "i" as an
+        // argument so that it is well defined at the point
+        // of calling the goroutine
         go func(i int) {
             fmt.Println(i)
             wg.Done()
