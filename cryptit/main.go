618f149dae19bc9ca1dc8485786da4957524ec47
Hasan <alkhashaab.h@gmail.com>
2024-10-07 21:07:47 +1030

    feat: created decrypt package

--- cryptit/main.go
+++ cryptit/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
     "fmt"
+    "github.com/codedByHasan/golang-for-beginners/decrypt"
     "github.com/codedByHasan/golang-for-beginners/encrypt"
 )
 
 func main() {
-    fmt.Println(encrypt.Nimbus("Hello World"))
+    encryptStr := encrypt.Nimbus("Hello World")
+    fmt.Println("Encrypted String: ", encryptStr)
+    fmt.Println("Decrypting the string...")
+    fmt.Println(decrypt.Nimbus(encryptStr))
 }
